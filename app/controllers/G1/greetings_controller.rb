33de93af538f6529ade9f3eb935aeb1cc73c9fc3
aubinleyoung <aubinsimpeze01@gmail.com>
2023-06-07 15:03:01 +0200

    Add  API endpoint

--- app/controllers/G1/greetings_controller.rb
+++
@@ -1,9 +0,0 @@
-module G1
-  # GreetingsController
-  class GreetingsController < ApplicationController
-    def index
-      greetings = Greeting.all
-      render json: greetings[rand(5)]
-    end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'static/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  
   # Defines the root path route ("/")
-  root 'static#index'
+  resources :greetings, only: [:index]
+  root "greetings#index"
 end
